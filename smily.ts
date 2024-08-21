47bece47f05c39edd90aad827ee7d63be8c827b2
Sorachat Patanasombat <sorachat.p@haupcar.com>
2024-08-22 00:40:47 +0700

    Finish implement countSmileys function

---
+++ smily.ts
@@ -0,0 +1,18 @@
+function countSmileys(input: string[]): number {
+    let result = 0
+
+    const isSmilyFace = (face: string): boolean => {
+        const regexSmilyPattern = /^(:|;){1}(-|~){0,1}(\)|D){1}$/g
+        return regexSmilyPattern.test(face)
+    }
+
+    for (const item of input) {
+        if (isSmilyFace(item)) result += 1
+    }
+
+    return result
+}
+
+console.log(countSmileys([':)', ';(', ';}', ':-D']))
+console.log(countSmileys([';D', ':-(', ':-)', ';~)']))
+console.log(countSmileys([';]', ':[', ';*', ':$', ';-D']))
