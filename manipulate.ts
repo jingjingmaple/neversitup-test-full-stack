525a60e588c9c15125d22f16b8f505f1f4dceb35
Sorachat Patanasombat <sorachat.p@haupcar.com>
2024-08-21 23:51:01 +0700

    Add pretier linting

--- manipulate.ts
+++ manipulate.ts
@@ -1,32 +1,32 @@
 function removeDuplicate(input: string[]) {
-  const init: string[] = [];
-  const uniqueArray = input.reduce((result, current) => {
-    if (!result.includes(current)) {
-      result.push(current);
-    }
-    return result;
-  }, init);
-  return uniqueArray;
+    const init: string[] = []
+    const uniqueArray = input.reduce((result, current) => {
+        if (!result.includes(current)) {
+            result.push(current)
+        }
+        return result
+    }, init)
+    return uniqueArray
 }
 
 function permutation(inputStr: string): string[] {
-  const inputArray: string[] = inputStr.split("");
-  let result: string[] = [];
+    const inputArray: string[] = inputStr.split('')
+    let result: string[] = []
 
-  for (let i = 0; i < inputArray.length; i++) {
-    if (inputArray.length == 1) return inputArray;
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray.length == 1) return inputArray
 
-    const removedChar = inputArray.shift() as string;
-    const resultStack = permutation(inputArray.join(""));
+        const removedChar = inputArray.shift() as string
+        const resultStack = permutation(inputArray.join(''))
 
-    for (let y = 0; y < resultStack.length; y++) {
-      resultStack[y] = `${resultStack[y]}${removedChar}`;
+        for (let y = 0; y < resultStack.length; y++) {
+            resultStack[y] = `${resultStack[y]}${removedChar}`
+        }
+        result = result.concat(resultStack)
+        inputArray.push(removedChar[0])
     }
-    result = result.concat(resultStack);
-    inputArray.push(removedChar[0]);
-  }
 
-  return removeDuplicate(result);
+    return removeDuplicate(result)
 }
-console.log(permutation("abc"));
-console.log(permutation("aabb"));
+console.log(permutation('abc'))
+console.log(permutation('aabb'))
